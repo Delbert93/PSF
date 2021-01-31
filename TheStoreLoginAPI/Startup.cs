6bbd3613154714d94b8c6743ae982261cd7d74e3
mckinnin <lloydmckinnin@gmail.com>
2021-01-30 23:53:52 -0700

    added allows for cors

--- TheStoreLoginAPI/Startup.cs
+++ TheStoreLoginAPI/Startup.cs
@@ -24,6 +24,14 @@ namespace TheStoreLoginAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
