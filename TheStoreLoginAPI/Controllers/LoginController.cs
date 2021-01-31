240a3fbcdf16e788af13f165723c631511576250
mckinnin <lloydmckinnin@gmail.com>
2021-01-30 19:22:53 -0700

    Got the basics in place for a temp db.. Also got a login front end set up in blazor.

--- TheStoreLoginAPI/Controllers/LoginController.cs
+++ TheStoreLoginAPI/Controllers/LoginController.cs
@@ -7,6 +7,8 @@ using TheStoreLoginAPI.Models;
 
 namespace TheStoreLoginAPI.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class LoginController : Controller
     {
         public IActionResult Index()
@@ -14,6 +16,7 @@ namespace TheStoreLoginAPI.Controllers
             return View();
         }
 
+        [HttpPost("[action]")]
         public async Task LoginValidation(string inputUsername, string inputPassword)
         {
             UserModel userModel = new UserModel();
@@ -22,12 +25,36 @@ namespace TheStoreLoginAPI.Controllers
 
             if(userModel.isValidUsername == true && userModel.isValidPassword == true)
             {
-                //check those values against the db
+                if(DBUserInfoLoginValidation(userModel))
+                {
+                    //tell UI "Login Successful;
+                }
+                else
+                {
+                    //tell the UI "Invalid Username or Password"
+                }
             }
             else
             {
                 //tell the UI "Invalid Username or Password"
             }
+        }
+
+        //in class validation until DB is set up
+        public bool DBUserInfoLoginValidation(UserModel userModel)
+        {
+            string AdminUsername = "XYZxyzwwwzyxzyx";
+            string AdminPassword = "abcxyzefgqrshijtuv";
+
+            if(userModel.getUsername() == AdminUsername && userModel.getPassword() == AdminPassword)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
     }
         
