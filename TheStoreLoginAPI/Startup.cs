2cf2e65430d3d28b526f9d994b74fece46404519
mckinnin <lloydmckinnin@gmail.com>
2021-02-01 14:14:59 -0700

    Removed not needed else statement

--- TheStoreLoginAPI/Startup.cs
+++ TheStoreLoginAPI/Startup.cs
@@ -44,12 +44,6 @@ namespace TheStoreLoginAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
