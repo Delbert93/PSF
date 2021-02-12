880c15be9b0ec24e292c881975f0bd746fe26b28
Stuart <nielsen.stuart9@gmail.com>
2021-02-11 18:45:43 -0700

    Snapshot model added, not used yet

---
+++ StoreLogin.Shared/UserSnapshot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreLogin.Shared
+{
+    public class UserSnapshot
+    {
+        private readonly string userName;
+        public int gameCredit { get; set; }
+    }
+}
