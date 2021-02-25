711df8d80367ff43f7d45e173a8a66ff384d27c0
Wyatt Brown <wyatt.brown23@gmail.com>
2021-02-25 09:14:45 -0700

    added a simple feature toggle for the navbar for navigation to scoreboard

--- TheStoreLoginAPI/Startup.cs
+++ TheStoreLoginAPI/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.FeatureManagement;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,8 @@ namespace TheStoreLoginAPI
             services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(convertUrlConnectionString(Configuration["DATABASE_URL"])));
             services.AddScoped<IRepository, EfCoreRepository>();
             services.AddRazorPages();
+
+            services.AddFeatureManagement();
 
             services.AddControllers();
 
