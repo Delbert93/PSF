259364d76c71157c36063e88f6a89c14ce781670
mckinnin <lloydmckinnin@gmail.com>
2021-03-05 14:48:52 -0700

    added check to see base address removed jitter giving us issues and not sure why yet.

--- TheStoreLoginFrontEnd/Program.cs
+++ TheStoreLoginFrontEnd/Program.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Polly;
-using Polly.Extensions.Http;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -17,7 +15,6 @@ namespace TheStoreLoginFrontEnd
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            var baseAddress = builder.Configuration["apiBaseAddress"];
             builder.RootComponents.Add<App>("app");
 
             Console.WriteLine($"apiBaseAddress: {builder.Configuration["apiBaseAddress"]}");
@@ -25,33 +22,7 @@ namespace TheStoreLoginFrontEnd
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["apiBaseAddress"]) });
             builder.Services.AddScoped<PublicApiService>();
 
-            builder.Services.AddHttpClient<PublicApiService>(builder.Configuration["apiBaseAddress"])
-               .SetHandlerLifetime(TimeSpan.FromMinutes(5))  //Sample. Default lifetime is 2 minutes
-               .AddPolicyHandler(GetRetryPolicy())
-               .AddPolicyHandler(GetCircuitBreakerPolicy());
-
-
             await builder.Build().RunAsync();
-        }
-
-        static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
-        {
-            Random jitterer = new Random();
-            var retryWithJitterPolicy = HttpPolicyExtensions
-                .HandleTransientHttpError()
-                .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
-                .WaitAndRetryAsync(6,    // exponential back-off plus some jitter
-                    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
-                                  + TimeSpan.FromMilliseconds(jitterer.Next(0, 100))
-                );
-            return retryWithJitterPolicy;
-        }
-
-        static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
-        {
-            return HttpPolicyExtensions
-                .HandleTransientHttpError()
-                .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
         }
     }
 }
