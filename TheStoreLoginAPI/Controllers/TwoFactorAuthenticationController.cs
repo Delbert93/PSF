9c4c5d0ecb57277b6736608b8670d630a37eae1b
McKinnin Lloyd <lloydmckinnin@gmail.com>
2021-03-20 20:56:48 -0600

    Started working on TwoFactorAuthentication but ran out of time and into problems. So does nothing right now.

---
+++ TheStoreLoginAPI/Controllers/TwoFactorAuthenticationController.cs
@@ -0,0 +1,43 @@
+﻿using Google.Authenticator;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreLogin.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheStoreLoginAPI.Data;
+
+namespace TheStoreLoginAPI.Controllers
+{
+    public class TwoFactorAuthenticationController : Controller
+    {
+        private readonly IRepository repository;
+
+        public TwoFactorAuthenticationController(IRepository repository)
+        {
+            this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<UserDTO>> Get() => await repository.Users.ToListAsync();
+
+        [HttpGet]
+        public ActionResult Enable()
+        {
+            UserDTO user = new UserDTO();
+            //UserDTO user = // TODO: fetch signed in user from a database
+            TwoFactorAuthenticator twoFactor = new TwoFactorAuthenticator();
+            var setupInfo =
+                twoFactor.GenerateSetupCode("myapp", user.Email, TwoFactorKey(user), false, 3);
+            ViewBag.SetupCode = setupInfo.ManualEntryKey;
+            ViewBag.BarcodeImageUrl = setupInfo.QrCodeSetupImageUrl;
+            return View();
+        }
+
+        private static string TwoFactorKey(UserDTO user)
+        {
+            return $"myverysecretkey+{user.Email}";
+        }
+    }
+}
